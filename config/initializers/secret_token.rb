47a5dafb8e6fb2e724ef38dd00b7754e2094846c
lyndzeblosser <lyndzeblosser@gmail.com>
2012-12-14 16:57:56 -0700

    Config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PckLaw::Application.config.secret_token = 'ab229ed470c4e35841fa27e19c42185d3c427dcb5aa6caed97700d708a668470436374984b3464a9d8f7b19ed063f8f0cf5b4803d1d7c93b1d59acbcff59a3d7'
